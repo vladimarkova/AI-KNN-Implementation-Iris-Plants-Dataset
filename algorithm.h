988a0740dcc057c979f1444c59508a55db8bd713
vladimarkova <vladislavam1996@gmail.com>
2021-01-16 13:51:32 +0200

    basic skeleton of the task

---
+++ algorithm.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
+void read_data() {
+
+}
+
+void classify_entry() {
+
+}
+
+void print_result_of_single_entry_classification() {
+    
+}
+
+void KNN() {
+
+}
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+int main() {
+    srand(time(0)); 
+
+    test();
+
+    return 0;
+}
